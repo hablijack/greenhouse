ca35c248b65cf60dfa7106ac44d5d1d77e5a7bd7
hablijack <christoph.habel@posteo.de>
2020-08-28 10:19:38 +0200

    Humidity sensor connected

--- lib/Humidity.py
+++ lib/Humidity.py
@@ -1,15 +1,21 @@
 ##!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#import sys
-#import Adafruit_DHT
-
+import time
+import board
+import adafruit_dht
 
 class Humidity():
 
     def read(self):
-        #DHT_TYPE = Adafruit_DHT.AM2302
-        #DHT_PIN  = 23
-        #humidity, temperature = Adafruit_DHT.read_retry(DHT_TYPE, DHT_PIN)
-        #return humidity
-        return ""
+        dhtDevice = adafruit_dht.DHT22(board.D26)
+        humidity = None
+        while humidity == None:
+            try:
+                humidity = dhtDevice.humidity
+            except RuntimeError as error:
+                # Errors happen fairly often, DHT's are hard to read, just keep going
+                print(error.args[0])
+                time.sleep(2.0)
+                continue
+        return humidity
