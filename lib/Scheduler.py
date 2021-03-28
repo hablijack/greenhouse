0dd7f2e725b09e681c5c34f7aaf701f1eea4ad91
hablijack <christoph.habel@posteo.de>
2021-03-28 17:16:09 +0200

    Watering as scheduled tasks

--- lib/Scheduler.py
+++ lib/Scheduler.py
@@ -11,6 +11,8 @@ from lib.sensors.Battery import Battery
 from lib.sensors.SoilTempInside import SoilTempInside
 from lib.sensors.AirTempOutside import AirTempOutside
 from lib.sensors.Gas import Gas
+from lib.MagnetValves import MagnetValves
+
 
 class Scheduler:
 
@@ -55,6 +57,28 @@ class Scheduler:
             trigger='interval',
             minutes=10)
 
+        self.scheduler.add_job(
+            id='start_watering',
+            func=self.start_watering1,
+            trigger='cron',
+            year='*',
+            month='*',
+            day='*',
+            hour='8-10,17-19',
+            minute='*/30',
+            second=0)
+
+        self.scheduler.add_job(
+            id='start_watering',
+            func=self.stop_watering1,
+            trigger='cron',
+            year='*',
+            month='*',
+            day='*',
+            hour='8-10,17-19',
+            minute='*/33',
+            second=0)
+
         self.scheduler.start()
         self.measure_all_values()
 
@@ -62,23 +86,33 @@ class Scheduler:
         json_body = [{
             "measurement": key,
             "time": timestamp,
-            "fields": { "value": value, "sensor": key }
+            "fields": {"value": value, "sensor": key}
         }]
         self.persistence.write(json_body)
+
+    def start_watering1(self):
+        MagnetValves().switch(1, True)
+
+    def stop_watering1(self):
+        MagnetValves().switch(1, False)
 
     def measure_battery_state(self):
         values = Battery().read()
         if values['battery_voltage']:
-            self.persist(datetime.now(), 'battery_voltage', values['battery_voltage'])
+            self.persist(datetime.now(), 'battery_voltage',
+                         values['battery_voltage'])
         if values['power_consumption']:
-            self.persist(datetime.now(), 'power_consumption', values['power_consumption'])
+            self.persist(datetime.now(), 'power_consumption',
+                         values['power_consumption'])
         if values['battery_capacity']:
-            self.persist(datetime.now(), 'battery_capacity', values['battery_capacity'])
+            self.persist(datetime.now(), 'battery_capacity',
+                         values['battery_capacity'])
 
     def measure_dht_sensor(self):
         values = HumidityAndTemperature().read()
         if values['temperature']:
-            self.persist(datetime.now(), 'air_temp_inside', values['temperature'])
+            self.persist(datetime.now(), 'air_temp_inside',
+                         values['temperature'])
         if values['humidity']:
             self.persist(datetime.now(), 'humidity_inside', values['humidity'])
 
