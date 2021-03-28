b6300faf29c8c47d08353aab755b99cc1b96ce20
hablijack <christoph.habel@posteo.de>
2021-03-28 13:20:54 +0200

    controller method added

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ from datetime import date
 
 app = Flask(__name__)
 
+
 @app.template_filter('iso8601_to_readable')
 def iso8601_to_readable(value):
     return dateutil.parser.parse(value).strftime('%H:%M:%S')
 
+
 @app.route('/assets/<path:path>')
 def send_assets(path):
     return send_from_directory('assets', path)
+
 
 @app.route('/')
 def overview():
@@ -26,17 +29,26 @@ def overview():
     short_history = persistence.get_short_history()
     return render_template('index.html', current=current, short_history=short_history)
 
+
+@app.route('/control')
+def history():
+    return render_template('control.html')
+
+
 @app.route('/history')
 def history():
     return render_template('history.html')
+
 
 @app.route('/history/air_temperature_inside')
 def air_temperature_inside():
     return jsonify(persistence.get_air_temperature_inside_history())
 
+
 @app.route('/history/air_temperature_outside')
 def air_temperature_outside():
     return jsonify(persistence.get_air_temperature_inside_history())
+
 
 if __name__ == '__main__':
     persistence = Persistence()
